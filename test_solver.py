284ab7c05a3221a94776ad83eba83c43fdc3d559
franlucc <fr.lucchetti@gmail.com>
2023-06-02 22:32:45 -0400

    test suite

--- test_solver.py
+++ test_solver.py
@@ -1,5 +1,9 @@
 from solver import Context
 
+'''
+Note: make sure timeout can cover all steps
+'''
+
 def test_time():
     c = Context()
     # NL command:
@@ -17,7 +21,7 @@ def test_time():
     assert(tp1 == 0), tp1
     assert(tp2 == 1), tp2
     
-def test_example1():
+def test_sat_1():
     """
     Go to Arjun's office, ask him if he is ready to head out, and come back and tell me what he said
 
@@ -34,20 +38,17 @@ def test_example1():
     
     start_loc = c.get_current_location()
     c.go_to("office")
-    assert(c.is_in_room("Arjun"))
     response = c.ask("Arjun", "ready to head out?", ["yes", "no"])
-    assert(response == "yes" or response == "no")
     c.go_to(start_loc)
-    assert(c.is_in_room("robot"))
     c.say("Arjun said: " + response)
     
     c.ctl.add(f':- not is_in_room("robot", "start_loc", {c.timeout}).')
     c.ctl.add(f':- not replied("Arjun", _, _).')
     (model, is_success) = c.ground_and_solve()
-    assert(is_success), (model, is_success)
+    assert(str(is_success) == "SAT"), (model, str(is_success))
     
 
-def test_example2():
+def test_sat_2():
     """
     Check if there are any mugs in the living room, and come back and tell me if there are any
     
@@ -75,9 +76,9 @@ def test_example2():
     c.ctl.add(':- not say("There is a mug in the living room", _).')
     c.ctl.add(f':- not is_in_room("robot", "start_loc", {c.timeout}).')
     (model, is_success) = c.ground_and_solve()
-    assert(is_success), (model, is_success)
+    assert(str(is_success) == "SAT"), (model, str(is_success))
     
-def test_example3():
+def test_sat_3():
     """
     Check if there are any staplers in the printer room
     
@@ -124,9 +125,9 @@ def test_example3():
     c.ctl.add(':- not say("There is no stapler in the house", _).')
     c.ctl.add(f':- not is_in_room("robot", "start_loc", {c.timeout}).')
     (model, is_success) = c.ground_and_solve()
-    assert(is_success), (model, is_success)
+    assert(str(is_success) == "SAT"), (model, str(is_success))
     
-def test_example4():
+def test_sat_4():
     """
     If I have a kitchen, tell Yash to make food. Otherwise, remind Yash that J2 closes at 9:00 pm.
     
@@ -140,24 +141,116 @@ def test_example4():
     go_to(start_loc)
     """
     init_state = ['is_in_room("robot", "start_loc", 0).']
-    c = Context(timeout=3, init_state=init_state)
+    c = Context(timeout=4, init_state=init_state)
     c.all_rooms.append("kitchen")
     
     list_of_rooms = c.get_all_rooms()
     start_loc = c.get_current_location()
-    if c.is_in_room("kitchen"):
+    if "kitchen" in list_of_rooms:
         c.go_to("kitchen")
         c.say("Yash, make food!")
     else:
-        c.say("Yash, no food")
+        c.say("Yash, J2 closes at 9:00 pm")
     c.go_to(start_loc)
         
-    c.ctl.add(':- not say("Yash, J2 closes at 9:00 pm").')
+    c.ctl.add(':- not say("Yash, make food!",_).')
     (model, is_success) = c.ground_and_solve()
-    assert(is_success), (model, is_success)
+    assert(str(is_success) ==  "SAT"), (model, str(is_success))
     
     
-def test_example5():
+def test_sat_5():
+    """
+    Go to Joe’s room and ask him for a number between 1 to 5. 
+    Do the same for Jill. Come to Yash’s room and tell them the
+    sum of the two chosen numbers. Everyone is in their respective rooms
+
+    def get_number(person):
+        start_loc = get_current_location()
+        go_to(person)
+        response = ask(person, "Choose number between 1 and 5", ["1","2","3","4","5"])
+        go_to(start_loc)
+        return response
+
+    start_loc = get_current_location()
+
+    joe_number = get_number("Joe")
+
+    jill_number = get_number("Jill")
+
+    sum_of_numbers = int(joe_number) + int(jill_number)
+
+    go_to("Yash's room")
+
+    say("The sum of Joe's number and Jill's number is " + str(sum_of_numbers))
+
+    go_to(start_loc)
+    """
+    init_state = ['is_in_room("robot", "start_loc", 0).', 
+                  'is_in_room("Joe", "Joe\'s room", 0).',
+                  'is_in_room("Jill", "Jill\'s room", 0).',
+                  'is_in_room("Yash", "Yash\'s room", 0).'
+    ]
+    c = Context(timeout=5, init_state=init_state)
+    c.all_rooms += ["Joe's room", "Jill's room", "Yash's room"]
+    
+    def get_number(person):
+        start_loc = c.get_current_location()
+        c.go_to(person)
+        response = c.ask(person, "Choose number between 1 and 5", ["1","2","3","4","5"])
+        c.go_to(start_loc)
+        return response
+    
+    start_loc = c.get_current_location()
+    joe_number = get_number("Joe")
+    jill_number = get_number("Jill")
+    sum_of_numbers = int(joe_number) + int(jill_number)
+    
+    c.go_to("Yash's room")
+    c.say("The sum of Joe's number and Jill's number is " + str(sum_of_numbers))
+    c.go_to(start_loc)
+    
+    c.ctl.add('''condition :- say("The sum of Joe\'s number and Jill\'s number is ''' + 
+              str(sum_of_numbers) + '''", T),
+              is_in_room("Yash", "Yash's room", T),
+              is_in_room("robot", "Yash's room", T).''')
+    c.ctl.add(':- not condition.')
+    (model, is_success) = c.ground_and_solve()
+    
+    assert(str(is_success) ==  "SAT"), (model, str(is_success))
+   
+    
+def test_unsat_1():
+    """
+    If I have a kitchen, tell Yash to make food. Otherwise, remind Yash that J2 closes at 9:00 pm.
+    
+    list_of_rooms = get_all_rooms()
+    start_loc = get_current_loc()
+    if "kitchen" in list_of_rooms:
+        go_to("kitchen")
+        say("Yash, make food!")
+    else:
+        say("Yash, no food")
+    go_to(start_loc)
+    """
+    init_state = ['is_in_room("robot", "start_loc", 0).']
+    c = Context(timeout=4, init_state=init_state)
+    c.all_rooms.append("living_room")
+    
+    list_of_rooms = c.get_all_rooms()
+    start_loc = c.get_current_location()
+    if "kitchen" in list_of_rooms:
+        c.go_to("kitchen")
+        c.say("Yash, make food!")
+    else:
+        c.say("Yash, J2 closes at 9:00 pm")
+    c.go_to(start_loc)
+        
+    c.ctl.add(':- not say("Yash, make food!",_).')
+    (model, is_success) = c.ground_and_solve()
+    assert(str(is_success) ==  "UNSAT"), (model, str(is_success))
+    
+      
+def test_unsat_2():
     """
     Find which room the cat is in, and come back and tell me.
     
@@ -176,21 +269,34 @@ def test_example5():
     return "The cat is not in the house"
     """
     init_state = ['is_in_room("robot", "start_loc", 0).', 'is_in_room("cat", "living_room", 0).']
-    c = Context(timeout=5, init_state=init_state)
+    c = Context(timeout=2, init_state=init_state)
     c.all_rooms.append("living_room")
-    
+    c.ctl.configuration.solve.parallel_mode = 8
+    c.ctl.configuration.solve.opt_mode = "ignore"
     start_loc = c.get_current_location()
+    
     list_of_rooms = c.get_all_rooms()
     
     if c.is_in_room("cat"):
         ret = "The cat is in the " + start_loc
-    
-    for room in list_of_rooms:
-        c.go_to(room)
-        if c.is_in_room("cat"):
-            ret = "The cat is in the " + room
-        c.go_to(start_loc)
+    else:  
+        for room in list_of_rooms:
+            c.go_to(room)
+            if c.is_in_room("cat"):
+                ret = "The cat is in the " + room
+                break
+            c.go_to(start_loc)
+        
         
     c.ctl.add(':- not say("The cat is in the living_room", _).')
     (model, is_success) = c.ground_and_solve()
-    assert(not is_success), (model, is_success)
+    assert(str(is_success) == "UNSAT"), (model, str(is_success))
+    
+def test_unsat_3():
+    pass
+
+def test_unsat_4():
+    pass
+
+def test_unsat_5():
+    pass
